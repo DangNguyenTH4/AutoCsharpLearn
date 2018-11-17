d35d3d0955b5ec91f4c724637245e9bf6b8381f1
DangNguyenTH4 <dangnt520@wru.vn>
2018-11-17 17:08:00 +0700

    Run a Program: cmd,notepad..,Open anyfile, Run backGround

---
+++ AutoCSharp/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace AutoCSharp
+{
+    partial class frmAutoCsharp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenNotepad = new System.Windows.Forms.Button();
+            this.btnRunCmdCommand = new System.Windows.Forms.Button();
+            this.btnRunCmdBack = new System.Windows.Forms.Button();
+            this.btnRunBG_WithResult = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpenNotepad
+            // 
+            this.btnOpenNotepad.Location = new System.Drawing.Point(13, 13);
+            this.btnOpenNotepad.Name = "btnOpenNotepad";
+            this.btnOpenNotepad.Size = new System.Drawing.Size(96, 23);
+            this.btnOpenNotepad.TabIndex = 0;
+            this.btnOpenNotepad.Text = "Open Notepad";
+            this.btnOpenNotepad.UseVisualStyleBackColor = true;
+            this.btnOpenNotepad.Click += new System.EventHandler(this.btnOpenNotepad_Click);
+            // 
+            // btnRunCmdCommand
+            // 
+            this.btnRunCmdCommand.Location = new System.Drawing.Point(116, 12);
+            this.btnRunCmdCommand.Name = "btnRunCmdCommand";
+            this.btnRunCmdCommand.Size = new System.Drawing.Size(118, 23);
+            this.btnRunCmdCommand.TabIndex = 1;
+            this.btnRunCmdCommand.Text = "Run Cmd Command";
+            this.btnRunCmdCommand.UseVisualStyleBackColor = true;
+            this.btnRunCmdCommand.Click += new System.EventHandler(this.btnRunCmdCommand_Click);
+            // 
+            // btnRunCmdBack
+            // 
+            this.btnRunCmdBack.Location = new System.Drawing.Point(60, 42);
+            this.btnRunCmdBack.Name = "btnRunCmdBack";
+            this.btnRunCmdBack.Size = new System.Drawing.Size(128, 23);
+            this.btnRunCmdBack.TabIndex = 2;
+            this.btnRunCmdBack.Text = "Run Cmd Background";
+            this.btnRunCmdBack.UseVisualStyleBackColor = true;
+            this.btnRunCmdBack.Click += new System.EventHandler(this.btnRunCmdBack_Click);
+            // 
+            // btnRunBG_WithResult
+            // 
+            this.btnRunBG_WithResult.Location = new System.Drawing.Point(12, 81);
+            this.btnRunBG_WithResult.Name = "btnRunBG_WithResult";
+            this.btnRunBG_WithResult.Size = new System.Drawing.Size(222, 23);
+            this.btnRunBG_WithResult.TabIndex = 3;
+            this.btnRunBG_WithResult.Text = "Run Cmd Background and Take out Result";
+            this.btnRunBG_WithResult.UseVisualStyleBackColor = true;
+            this.btnRunBG_WithResult.Click += new System.EventHandler(this.btnRunBG_WithResult_Click);
+            // 
+            // frmAutoCsharp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 450);
+            this.Controls.Add(this.btnRunBG_WithResult);
+            this.Controls.Add(this.btnRunCmdBack);
+            this.Controls.Add(this.btnRunCmdCommand);
+            this.Controls.Add(this.btnOpenNotepad);
+            this.Name = "frmAutoCsharp";
+            this.Text = "AutoCsharp";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenNotepad;
+        private System.Windows.Forms.Button btnRunCmdCommand;
+        private System.Windows.Forms.Button btnRunCmdBack;
+        private System.Windows.Forms.Button btnRunBG_WithResult;
+    }
+}
+
---
+++ AutoCSharp/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoCSharp
+{
+    public partial class frmAutoCsharp : Form
+    {
+        public frmAutoCsharp()
+        {
+            InitializeComponent();
+        }
+
+        //Open any File/Program/...
+        private void btnOpenNotepad_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"D:\Anh ky yeu mai\anh Mai\DSC03892.JPG");
+            Process.Start("notepad");
+            Process.Start("cmd");   
+            //Process.Start(AppDomain.CurrentDomain.BaseDirectory + "");
+        }
+
+        private void btnRunCmdCommand_Click(object sender, EventArgs e)
+        {
+            #region Cach 1
+            //string strCmdCm;
+            //strCmdCm = "/C ping -t google.com";
+            //Process.Start("cmd", strCmdCm);
+            #endregion
+            #region Cach 2_ cach chuan
+            string strCmdCm;
+            strCmdCm = "/C ping -t google.com";
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd";
+            p.StartInfo.Arguments = strCmdCm;
+            p.Start();
+            p.Kill();
+            #endregion
+        }
+
+        private void btnRunCmdBack_Click(object sender, EventArgs e)
+        {
+            string cm = @"/C ""Ha ha ha.jpg""";
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd";
+            p.StartInfo.Arguments = cm;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.Start();
+
+            //....
+            //p.Kill();
+        }
+
+        private void btnRunBG_WithResult_Click(object sender, EventArgs e)
+        {
+            string cmdCommand = "/C ping google.com";
+            string fileName = "cmd";
+
+            // Create instance Process
+            Process p = new Process();
+
+            //Create instace StartInfo, setting it!
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = fileName;
+            startInfo.Arguments = cmdCommand;
+            //startInfo.CreateNoWindow = true; //Set hidden cmd Window
+            startInfo.UseShellExecute = false;
+           startInfo.RedirectStandardInput = true;
+            startInfo.RedirectStandardOutput = true;
+
+            //Start Process
+            p.StartInfo = startInfo;
+            p.Start();
+
+            //Take output
+            
+            p.StandardInput.WriteLine(cmdCommand);
+            p.StandardInput.Flush();
+            p.StandardInput.Close();
+            p.WaitForExit();
+            
+            string result = p.StandardOutput.ReadToEnd();
+            MessageBox.Show(result);
+        }
+    }
+}
